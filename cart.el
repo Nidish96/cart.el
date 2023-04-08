a8519ae11b4ee042bd21865efc4fd7382d63e5ea
nidish <nidbid@gmail.com>
2023-04-08 22:39:25 +0200

    We now support both rotations and translations of tikz objects

--- cart.el
+++ cart.el
@@ -1,5 +1,5 @@
 (defcustom cart--XY_0sl '((X . (0.0 1.0))
-			(Y . (0.0 1.0)))
+                        (Y . (0.0 1.0)))
   "These are the calibration values"
   :group 'cart)
 
@@ -13,15 +13,16 @@
 
 (define-minor-mode cart-mode
   "Automatic inteRactive coordinates for Tikz"
-  nil
   :global nil
   :group 'cart
   :lighter " cart"
   :keymap
   (list (cons (cart--key "c") #'cart-calibrate)
-	(cons (cart--key "p") #'cart-insert-point)
-	(cons (cart--key "d") #'cart-tikz-draw)
-	(cons (cart--key "n") #'cart-tikz-node))
+        (cons (cart--key "p") #'cart-insert-point)
+        (cons (cart--key "d") #'cart-tikz-draw)
+        (cons (cart--key "n") #'cart-tikz-node)
+        (cons (cart--key "t") #'cart-translate-tikz)
+        (cons (cart--key "r") #'cart-rotate-tikz))
 
   (if cart-mode
       (message "cart-mode activated!")
@@ -81,7 +82,7 @@ start and end points of dragged region (if dragged) as a list of lists."
     (list XY1 XY2 xy1 xy2)
     ))
 
-(defun cart/XY2xy (XY)
+(defun cart--XY2xy (XY)
   "Transform point from pixels to calibrated coordinate system"
   (list
    (/ (- (elt XY 0) (elt (alist-get 'X cart--XY_0sl) 0)) (elt (alist-get 'X cart--XY_0sl) 1))
@@ -91,9 +92,8 @@ start and end points of dragged region (if dragged) as a list of lists."
   "Query point and insert coordinates"
   (interactive)
   (let ((XY (cart--gmc "Click on Point")))
-    (message "%s" XY)
     (if XY (progn
-             (setq xy (cart/XY2xy XY))
+             (setq xy (cart--XY2xy XY))
              (insert (format "(%f, %f)" (elt xy 0) (elt xy 1)))
              xy))))
 
@@ -120,24 +120,33 @@ start and end points of dragged region (if dragged) as a list of lists."
 
 (defun cart--last-open-paren (&optional pos)
   "Returns the last open paren that the current point lies in.
-Optional argument POS allows user to specify point (other that current).
+  Optional argument POS allows user to specify point (other that current).
 
-Code from this stackoverflow answer: https://emacs.stackexchange.com/a/10405"
+  Code from this stackoverflow answer: https://emacs.stackexchange.com/a/10405"
   (let ((ppss (syntax-ppss (or pos (point)))))
     (when (nth 1 ppss) (char-after (nth 1 ppss)))))
 
 (defun cart--goto-begend (&optional enflg)
   (if enflg
-      (while (cart--last-open-paren (search-forward ";")))
-    (while (cart--last-open-paren (search-backward "\\"))))
+      (while (cart--last-open-paren (search-forward ";" nil t)))
+    (while (cart--last-open-paren (search-backward "\\" nil t))))
   (point))
 
-(defun cart--translate (&optional dx dy) 
-  "Conduct rigid body movement on current object.
-DX, DY are x (horizontal) and y (vertical translation.
+(defun cart--angle (vec1 vec2)
+  "Returns the angle between the two vectors (given as lists)."
+  (let ((A1 (sqrt (apply '+ (mapcar #'(lambda (a) (expt a 2)) vec1))))
+        (A2 (sqrt (apply '+ (mapcar #'(lambda (a) (expt a 2)) vec2)))))
+    (setq Cth (/ (+ (* (elt vec1 0) (elt vec2 0))
+                    (* (elt vec1 1) (elt vec2 1)))
+                 (* A1 A2)))
+    (setq Sth (/ (- (* (elt vec1 0) (elt vec2 1))
+                    (* (elt vec2 0) (elt vec1 1)))
+                 (* A1 A2)))
+    (atan Sth Cth)))
 
-BUG: sentence doesn't seem to mean what we expected.
-Do we want to include rotations also?"
+(defun cart--translate (&optional dx dy) 
+  "Conduct rigid body translation on current object.
+  DX, DY are x (horizontal) and y (vertical translation."
   (goto-char (point-min))
   (while (setq p0 (search-forward "(" (point-max) t))
     (if (cart--last-open-paren (1- p0))
@@ -146,15 +155,61 @@ Do we want to include rotations also?"
       (setq cds
             (mapcar 'string-to-number
                     (split-string
-                     (buffer-substring p0 p1) ",")))
+                     (replace-regexp-in-string
+                      "\n" "" (buffer-substring p0 p1))
+                     ",")))
       (delete-region p0 p1)
       (goto-char p0)
       (setf (elt cds 0) (+ (elt cds 0) (or dx 0)))
       (setf (elt cds 1) (+ (elt cds 1) (or dy 0)))
       (insert (mapconcat 'number-to-string cds ",")))))
 
-(defun cart-move-object ()
-  "Move objects in current sentence or under region using two points."
+(defun cart--rotate (&optional tht cpt rnds) 
+  "Conduct rigid body rotation on current object.
+THT is rotation angle.
+CPT is a list storing center point.
+RNDS is a boolean governing whether node contents should be rotated or not."
+  (goto-char (point-min))
+  (while (setq p0 (search-forward "(" (point-max) t))
+    (if (cart--last-open-paren (1- p0))
+        (goto-char (1+ (point)))
+      (setq p1 (1- (search-forward ")")))
+      (setq cds
+            (mapcar 'string-to-number
+                    (split-string
+                     (replace-regexp-in-string
+                      "\n" "" (buffer-substring p0 p1))
+                     ",")))
+      (delete-region p0 p1)
+      (goto-char p0)
+      ;; Relative coordinates & Rotation
+      (setq cdsrel (list (- (elt cds 0) (or (elt cpt 0) 0))
+                         (- (elt cds 1) (or (elt cpt 1) 0))))
+      (setq Cth (cos (or tht 0)))
+      (setq Sth (sin (or tht 0)))
+      (setq Tcds (list (+ (- (* Cth (elt cdsrel 0)) (* Sth (elt cdsrel 1))) (or (elt cpt 0) 0))
+                       (+ (+ (* Sth (elt cdsrel 0)) (* Cth (elt cdsrel 1))) (or (elt cpt 1) 0))))
+      (insert (mapconcat 'number-to-string Tcds ","))))
+  ;; Rotate nodes too, if needed
+  (when rnds
+    (goto-char (point-min))
+    (while (search-forward "node" nil t)
+      (if (not (eq (char-after) (string-to-char "[")))
+          (insert (format "[rotate=%f]" (radians-to-degrees tht)))
+        (setq ebr (save-excursion (search-forward "]")))
+        (if (search-forward "rotate" ebr t)
+            (progn
+              (right-word)
+              (setq nwang (+ (number-at-point) (radians-to-degrees tht)))
+              (skip-chars-backward "0-9.-")
+              (delete-region (point) (progn (skip-chars-forward "0-9.-") (point)))
+              (insert (format "%f" nwang))
+              (goto-char ebr))
+          (goto-char (1- ebr))
+          (insert (format ", rotate=%f" (radians-to-degrees tht))))))))
+
+(defun cart-translate-tikz ()
+  "Translate objects in current sentence or under region using two points."
   (interactive)
   (let* ((XYs (cart--gmc "Click & drag from start point to end point"))
          (XY0 (elt XYs 0))
@@ -162,8 +217,8 @@ Do we want to include rotations also?"
     (unless (listp XY0)
       (setq XY0 XYs)
       (setq XY1 (cart--gmc "You had only clicked on one point. Please click target point now")))
-    (setq xy0 (cart/XY2xy XY0))
-    (setq xy1 (cart/XY2xy XY1))
+    (setq xy0 (cart--XY2xy XY0))
+    (setq xy1 (cart--XY2xy XY1))
 
     (setq dx (- (elt xy1 0) (elt xy0 0)))
     (setq dy (- (elt xy1 1) (elt xy0 1)))
@@ -173,7 +228,46 @@ Do we want to include rotations also?"
       (narrow-to-region (cart--goto-begend) (cart--goto-begend t)))
 
     (cart--translate dx dy)
-    (widen)
-    ))
+    (goto-char (point-min))
+    (while (not (eobp))
+      (move-end-of-line nil)
+      (do-auto-fill)
+      (forward-line))
+    (do-auto-fill)
+    (widen)))
+
+(defun cart-rotate-tikz ()
+  "Rotate objects in current sentence or under region using a reference point and two target points."
+  (interactive)
+  (let* ((XYref (cart--gmc "Click on the center of rotation (RET to use origin) "))
+         (XYs (cart--gmc "Click and drag the rotation target points "))
+         (rnds (y-or-n-p "Rotate Node contents too?"))
+         (XY0 (elt XYs 0))
+         (XY1 (elt XYs 1)))
+    (setq xyref (if XYref (cart--XY2xy XYref) (list 0 0)))
+    (unless (listp XY0)
+      (setq XY0 XYs)
+      (setq XY1 (cart--gmc "You had only clicked on one point. Please click target point now")))
+
+    (setq xy0 (cart--XY2xy XY0))
+    (setq xy1 (cart--XY2xy XY1))
+
+    ;; Relative Coordinates
+    (setq xy0 (list (- (elt xy0 0) (elt xyref 0)) (- (elt xy0 1) (elt xyref 1))))
+    (setq xy1 (list (- (elt xy1 0) (elt xyref 0)) (- (elt xy1 1) (elt xyref 1))))
+    (setq theta (cart--angle xy0 xy1))
+
+    (if (region-active-p)
+        (narrow-to-region (region-beginning) (region-end))
+      (narrow-to-region (cart--goto-begend) (cart--goto-begend t)))
+
+    (cart--rotate theta xyref rnds)
+    (goto-char (point-min))
+    (while (not (eobp))
+      (move-end-of-line nil)
+      (do-auto-fill)
+      (forward-line))
+    (do-auto-fill)
+    (widen)))
 
 (provide 'cart)
